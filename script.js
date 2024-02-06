633c2e762952bf08474ef6b810b085ca5988e27d
Ege Nazmi <egenazmia@gmail.com>
2024-02-06 12:44:46 +0300

    Navbar Position Changed to Fixed

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const hamburger=document.querySelector('.hamburger')
-
+const bar1 = document.querySelector('.bar1')
+const bar2 = document.querySelector('.bar2')
+const bar3 = document.querySelector('.bar3')
 
 hamburger.addEventListener('click', ()=>{
-    document.querySelector('.bar1').classList.toggle('animationBar1')
-    document.querySelector('.bar2').classList.toggle('animationBar2')
-    document.querySelector('.bar3').classList.toggle('animationBar3')
+    bar1.classList.toggle('animationBar1')
+    bar2.classList.toggle('animationBar2')
+    bar3.classList.toggle('animationBar3')
 })
 
+const navlinks = document.querySelector('.nav-links')
 hamburger.addEventListener('click', ()=>{
-    document.querySelector('.nav-links').classList.toggle('openSideBar')
+    navlinks.classList.toggle('openSideBar')
 })
